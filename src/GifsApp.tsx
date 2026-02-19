e42eeed2cf170b24e5aea6a865d92a5642c12f50
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-18 22:11:58 -0500

    feat: add component GifList

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,6 +1,8 @@
 import { PreviousSearches } from "./gifs/components/PreviousSearches";
 import { CustomHeader } from './shared/components/CustomHeader';
 import { SearchBar } from "./shared/components/SearchBar";
+import { GifList } from "./gifs/components/GifList";
+
 import { mockGifs } from "./mock-data/gifs.mock";
 
 export const GifsApp = () => {
@@ -9,21 +11,11 @@ export const GifsApp = () => {
       {/* header */}
       <CustomHeader title="Gifs search" description="Discover and share the perfect gif" />
       {/* search container */}
-      <SearchBar />
+      <SearchBar placeholder="Search anything"/>
       {/* previous searches */}
-      <PreviousSearches />
+      <PreviousSearches searches={['Goku', 'Test', 'Dev']}/>
       {/* gifs container */}
-      <div className="gifs-container">
-        {
-          mockGifs.map((gif) => (
-            <div className="gif-card" key={gif.id}>
-              <img src={gif.url} alt={gif.title} />
-              <h3>{gif.title}</h3>
-              <p>{gif.width}x{gif.height} (1.5mb)</p>
-            </div>
-          ))
-        }
-      </div>
+      <GifList gifs={mockGifs} />
     </>
   );
 }
