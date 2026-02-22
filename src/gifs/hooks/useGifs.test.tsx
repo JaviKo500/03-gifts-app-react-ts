5c5a953af39f7e86d8d2e847e6485c215d4b2396
JaViKo500 <javikogutierrez64@gmail.com>
2026-02-22 09:42:41 -0500

    test: add test in cache previous gifs

--- src/gifs/hooks/useGifs.test.tsx
+++ src/gifs/hooks/useGifs.test.tsx
@@ -1,7 +1,9 @@
 import { renderHook } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
 import { useGifs } from './useGifs';
 import { act } from 'react';
+import * as gifsAction from '../actions/get-gifs-by-query.action';
+
 describe('UseGifs.test', () => {
   test( 'should return default values and methods/actions', () => {
     const { result } = renderHook( () => useGifs() );
@@ -32,5 +34,59 @@ describe('UseGifs.test', () => {
 
     expect( result.current.gifts.length ).toBeGreaterThan(0);
     expect( result.current.previousTerms.length ).toBeGreaterThan(0);
+  });
+
+  test( 'should return a list of gifs from cache', async () => {
+    
+    const { result } = renderHook( () => useGifs() );
+    await act( async () => {
+      await result.current.handleTermClick('goku');
+    });
+
+    vi.spyOn( gifsAction, 'getGifsByQueryAction' )
+      .mockRejectedValue( new Error('This is mock error') );
+      
+    await act( async () => {
+      await result.current.handleTermClick('goku');
+    });
+
+    expect( result.current.gifts.length ).toBeGreaterThan(0);
+  });
+
+  test( 'should return no more than 8 previous terms', async () => {
+    const { result } = renderHook( () => useGifs() );
+    vi.spyOn( gifsAction, 'getGifsByQueryAction' )
+      .mockResolvedValue( [] );
+    await act( async () => {
+      await result.current.handleSearch('goku');
+    });
+    await act( async () => {
+      await result.current.handleSearch('naruto');
+    });
+    await act( async () => {
+      await result.current.handleSearch('sasuke');
+    });
+    await act( async () => {
+      await result.current.handleSearch('jiraiya');
+    });
+    await act( async () => {
+      await result.current.handleSearch('kakashi');
+    });
+    await act( async () => {
+      await result.current.handleSearch('saitama');
+    });
+    await act( async () => {
+      await result.current.handleSearch('dog');
+    });
+    await act( async () => {
+      await result.current.handleSearch('cat');
+    });
+    await act( async () => {
+      await result.current.handleSearch('auron');
+    });
+
+    expect( result.current.previousTerms.length ).toBe(8);
+    expect( result.current.previousTerms.at(0) ).toBe('auron');
+    expect( result.current.previousTerms.at(-1) ).toBe('naruto');
   });
 });
